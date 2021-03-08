1919fee93aa4042d0fa635b50439cce0a4a203ca
Chua Kang Ming <kambing860210@gmail.com>
2021-03-08 21:04:22 +0800

    fix: unit test
    feat: add travis config

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,25 @@
+import { ThemeProvider } from "@material-ui/core";
 import { render, screen } from "@testing-library/react";
+import { useAppTheme } from "hooks/useAppTheme";
 import React from "react";
 import App from "./App";
 
-test("renders learn react link", () => {
+jest.mock("@material-ui/core");
+jest.mock("hooks/useAppTheme");
+jest.mock("components/MainLayout");
+
+const mockedThemeProvider = ThemeProvider as jest.Mock;
+const mockedUseAppTheme = useAppTheme as jest.Mock;
+
+test("renders ThemeProvider", () => {
+  mockedThemeProvider.mockImplementation(({ theme }: { theme: string }) => {
+    expect(theme).toBe("a");
+    return <div>Weather app</div>;
+  });
+  mockedUseAppTheme.mockReturnValue({ theme: "a" });
   render(<App />);
   const linkElement = screen.getByText(/Weather app/i);
   expect(linkElement).toBeInTheDocument();
+  expect(mockedThemeProvider).toBeCalledTimes(1);
+  expect(mockedUseAppTheme).toBeCalledTimes(1);
 });
