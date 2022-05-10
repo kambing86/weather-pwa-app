19c1b6df3c376a9221cb2f7c7ad6c4c89074a55f
Chua Kang Ming <chuakangming@spgroup.com.sg>
2022-05-10 17:59:07 +0800

    chore: fix nvm issue in husky

--- .husky/common.sh
+++ .husky/common.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-
-nvm use
-echo "nvm version: $(nvm --version)"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" && nvm use && echo "nvm version: $(nvm --version)"
 echo "node version: $(node --version)"
 echo "pnpm version: $(pnpm --version)"
