f70a59c5a5ad8e11ffae190f3af1830671b0911c
Chua Kang Ming <kambing860210@gmail.com>
2022-05-31 21:58:23 +0800

    chore: update packages

--- .husky/common.sh
+++ .husky/common.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" && nvm use && echo "nvm version: $(nvm --version)"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" && echo "nvm version: $(nvm --version)" && nvm use
 echo "node version: $(node --version)"
 echo "pnpm version: $(pnpm --version)"
