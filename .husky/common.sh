fe6ab37c479c4fe2a9132f5f1eaf28bc697bcfdc
Chua Kang Ming <kambing860210@gmail.com>
2023-11-05 22:57:36 +0800

    fix: husky issue

--- .husky/common.sh
+++ .husky/common.sh
@@ -2,4 +2,4 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" && echo "nvm version: $(nvm --version)" && nvm use
 echo "node version: $(node --version)"
-echo "pnpm version: $(pnpm --version)"
+echo "yarn version: $(yarn --version)"
